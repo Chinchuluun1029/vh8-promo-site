3fac95a9a08441f1539dbf03024f18e0585c8469
Chinchuluun1029 <chinchuluun1029@gmail.com>
2022-02-20 17:52:18 -0600

    add assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,8 +8,10 @@ module.exports = {
       },
       assert: {
         assertions: {
-          "categories:performance": ["warn", {"minScore": 0.97}],
-          "categories:accessibility": ["warn", {"minScore": 0.9}]
+          "categories:performance": ["warn", {"minScore": 0.8}],
+          "categories:accessibility": ["warn", {"minScore": 0.75}],
+          "categories:bestPractices": ["warn", {"minScore": 0.75}],
+          "categories:seo": ["warn", {"minScore": 0.75}]
         }
       },
     },
