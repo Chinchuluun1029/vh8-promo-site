03bb0b1747b131df98fe93930516c1e1e82d880f
Chinchuluun1029 <chinchuluun1029@gmail.com>
2022-02-21 01:47:49 -0600

    update and format lh rc

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,13 +1,18 @@
 module.exports = {
-    ci: {
-      collect: {
-        staticDistDir: './',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
-      assert: {
-        preset: 'lighthouse:recommended',
+  ci: {
+    collect: {
+      staticDistDir: "./public/",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.8 }],
+        "categories:accessibility": ["warn", { minScore: 0.75 }],
+        "categories:bestPractices": ["warn", { minScore: 0.75 }],
+        "categories:seo": ["warn", { minScore: 0.75 }],
       },
     },
-  };
+  },
+};
